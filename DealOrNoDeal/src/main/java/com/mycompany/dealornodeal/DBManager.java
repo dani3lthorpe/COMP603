6a4abb87df643b554f84aec8d3ae21deeb1b8e1a
dani3lthorpe <dani3lthorpe@gmail.com>
2024-05-14 14:22:37 +1200

    Created DB manager

---
+++ DealOrNoDeal/src/main/java/com/mycompany/dealornodeal/DBManager.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dealornodeal;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author hidan
+ */
+public class DBManager {
+
+    private static final String USER_NAME = "group69";
+    private static final String PASSWORD = "abcd";
+    private static final String URL = "jdbc:derby:DealOrNoDealDB_Ebd;create=true";
+
+    Connection conn;
+
+    public DBManager() {
+        establishConnection();
+    }
+
+    private void establishConnection() {
+        try {
+            conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            System.out.println(URL + " connected...");
+        } catch (SQLException ex) {
+            System.err.println("SQLException: " + ex.getMessage());
+        }
+    }
+
+    public void closeConnections() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+}
