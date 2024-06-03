5cf68d87d0105376f5932b0588e2b05ab0de91e2
dani3lthorpe <dani3lthorpe@gmail.com>
2024-06-03 22:22:00 +1200

    Added tests

--- DealOrNoDeal/test/com/mycompany/dealornodeal/GameModeTest.java
+++ DealOrNoDeal/test/com/mycompany/dealornodeal/GameModeTest.java
@@ -47,43 +47,39 @@ public class GameModeTest {
      */
     @Test
     public void testSetCases() {
-        ArrayList<Integer> casePrizes = new ArrayList<>();
-        for (int prize : gameModeTest.getPrizes()) {
-            casePrizes.add(prize);
-        }
-
-        assertEquals(gameModeTest.getCases().size(), casePrizes.size());
+        assertEquals(gameModeTest.getCases().size(), gameModeTest.getPrizes().size());
         for (int prize : gameModeTest.getPrizes()) {
             boolean found = false;
             for (Case c : gameModeTest.getCases()) {
                 if (c.getPrize() == prize) {
                     found = true;
-                    break;
                 }
             }
-            
             assertTrue(found);
         }
     }
 
     /**
-     * Test of removePrize method, of class GameMode.
+     * Test of removePrize method, of class GameMode. Tests if prize is removed
+     * from array
      */
     @Test
     public void testRemovePrize() {
         System.out.println("removePrize");
         int prize = gameModeTest.getCases().get(2).getPrize();
-        gameModeTest.removePrize(gameModeTest.getCases().get(2).getNumber());
-        ArrayList<Case> cases = gameModeTest.getCases();
-        for (Case c : cases) {
-            assertNotEquals(prize, c.getPrize());
+        gameModeTest.removePrize(2);
+        ArrayList<Integer> prizes = gameModeTest.getPrizes();
+        for (int amount : prizes) {
+            assertNotEquals(prize, amount);
         }
     }
 
     /**
-     * Test of displayOffer method, of class GameMode.
+     * Test of acceptOffer method, of class GameMode. Tests if offer is Smaller
+     * than the highest Prizes if it doesn't save the offer
      */
     @Test
+<<<<<<< Updated upstream
     public void testDisplayOffer() {
         System.out.println("displayOffer");
         GameMode instance = null;
@@ -244,7 +240,67 @@ public class GameModeTest {
         }
 
         public void newRound() {
+=======
+    public void testAcceptSmallerOffer() {
+        System.out.println("acceptSmallerOffer");
+        gameModeTest.getGameData().setCurrentOffer(-10);
+        gameModeTest.acceptOffer();
+        for (int highPrize : gameModeTest.getPlayer().getHighPrizes()) {
+            assertNotEquals(highPrize, -10);
+>>>>>>> Stashed changes
         }
     }
+
+    /**
+     * Test of acceptOffer method, of class GameMode. Tests if offer is Bigger
+     * than the highest Prizes if it saves it to highest prizes
+     */
+    @Test
+    public void testAcceptBiggerOffer() {
+        System.out.println("acceptBiggerOffer");
+        gameModeTest.getGameData().setCurrentOffer(5000000);
+        gameModeTest.acceptOffer();
+        boolean found = false;
+        for (int highPrize : gameModeTest.getPlayer().getHighPrizes()) {
+            if (highPrize == 5000000) {
+                found = true;
+            }
+        }
+        assertTrue(found);
+    }
+
+    /**
+     * Test of openYourCase method, of class GameMode. Checks if it saves the
+     * prize to highest prizes if it is bigger
+     */
+    @Test
+    public void testOpenYourCaseBiggerPrize() {
+        System.out.println("openYourCaseBiggerPrize");
+        Case testCase = new Case(0, 5000000);
+        gameModeTest.getGameData().setPlayerCase(testCase);
+        gameModeTest.openYourCase();
+        Boolean found = false;
+        for (int highPrize : gameModeTest.getPlayer().getHighPrizes()) {
+            if (highPrize == 5000000) {
+                found = true;
+            }
+        }
+        assertTrue(found);
+    }
+
+    /**
+     * Test of openYourCase method, of class GameMode. Checks if it doesn't save
+     * the prize to highest prizes if it is smaller
+     */
+    @Test
+    public void testOpenYourCaseSmallerPrize() {
+        System.out.println("openYourCaseSmallerPrize");
+        Case testCase = new Case(0, 5000000);
+        gameModeTest.getGameData().setPlayerCase(testCase);
+        for (int highPrize : gameModeTest.getPlayer().getHighPrizes()) {
+            assertNotEquals(highPrize, -10);
+        }
+    }
+
 
 }
